0bf90eb6c8c72aab473db88881999e08fd6279f0
Afroz <afroz9045@gmail.com>
2022-07-27 16:42:52 +0530

    filter data for specific subjects

--- LINQ/LinqPlayground/LinqPlayground/Data.cs
+++ LINQ/LinqPlayground/LinqPlayground/Data.cs
@@ -11,23 +11,23 @@ namespace LinqPlayground
         public string First { get; set; }
         public string Last { get; set; }
         public int ID { get; set; }
-        public List<int> Scores;
+        public Dictionary<string,int> Scores;
         public static List<Student> GetStudents()
         {
             return new List<Student>{
-    new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
-    new Student {First="Claire", Last="O'Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
-    new Student {First="Sven", Last="Mortensen", ID=113, Scores= new List<int> {88, 94, 65, 91}},
-    new Student {First="Cesar", Last="Garcia", ID=114, Scores= new List<int> {97, 89, 85, 82}},
-    new Student {First="Debra", Last="Garcia", ID=115, Scores= new List<int> {35, 72, 91, 70}},
-    new Student {First="Fadi", Last="Fakhouri", ID=116, Scores= new List<int> {99, 86, 90, 94}},
-    new Student {First="Hanying", Last="Feng", ID=117, Scores= new List<int> {93, 92, 80, 87}},
-    new Student {First="Hugo", Last="Garcia", ID=118, Scores= new List<int> {92, 90, 83, 78}},
-    new Student {First="Lance", Last="Tucker", ID=119, Scores= new List<int> {68, 79, 88, 92}},
-    new Student {First="Terry", Last="Adams", ID=120, Scores= new List<int> {99, 82, 81, 79}},
-    new Student {First="Eugene", Last="Zabokritski", ID=121, Scores= new List<int> {96, 85, 91, 60}},
-    new Student {First="Michael", Last="Tucker", ID=122, Scores= new List<int> {94, 92, 91, 91}},
-    new Student {First="Michael", Last="Fastbender", ID=122, Scores= new List<int> {94, 92, 91, 91}}
+    new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new Dictionary<string,int> { {"Maths",97 }, {"Science", 92 }, {"Bio", 81}, {"Social", 60 } } },
+    new Student {First="Claire", Last="O'Donnell", ID=112, Scores= new Dictionary<string,int> { { "Maths", 75 }, { "Science", 84 }, { "Bio", 91 }, { "Social", 39 } }},
+    new Student {First="Sven", Last="Mortensen", ID=113, Scores= new Dictionary<string,int> { { "Maths", 88 }, { "Science", 94 }, { "Bio", 65 }, { "Social", 91 } }},
+    new Student {First="Cesar", Last="Garcia", ID=114, Scores= new Dictionary<string,int> { { "Maths", 97 }, { "Science", 89 }, { "Bio", 85 }, { "Social", 82 } }},
+    new Student {First="Debra", Last="Garcia", ID=115, Scores= new Dictionary<string,int> { { "Maths", 35 }, { "Science", 72 }, { "Bio", 91 }, { "Social", 70 } }},
+    new Student {First="Fadi", Last="Fakhouri", ID=116, Scores= new Dictionary<string,int> { { "Maths", 99 }, { "Science", 86 }, { "Bio", 90 }, { "Social", 94 } }},
+    new Student {First="Hanying", Last="Feng", ID=117, Scores= new Dictionary<string,int> { { "Maths", 93 }, { "Science", 92 }, { "Bio", 80 }, { "Social", 87 } }},
+    new Student {First="Hugo", Last="Garcia", ID=118, Scores= new Dictionary<string,int> { { "Maths", 92 }, { "Science", 90 }, { "Bio", 83 }, { "Social", 78 } }},
+    new Student {First="Lance", Last="Tucker", ID=119, Scores= new Dictionary<string,int> { { "Maths", 68 }, { "Science", 79 }, { "Bio", 88 }, { "Social", 92 } }},
+    new Student {First="Terry", Last="Adams", ID=120, Scores= new Dictionary<string,int> { { "Maths", 99 }, { "Science", 82 }, { "Bio", 81 }, { "Social", 79 } }},
+    new Student {First="Eugene", Last="Zabokritski", ID=121, Scores= new Dictionary<string,int> { { "Maths", 96 }, { "Science", 85 }, { "Bio", 91 }, { "Social", 60 } }},
+    new Student {First="Michael", Last="Tucker", ID=122, Scores= new Dictionary<string,int> { { "Maths", 94 }, { "Science", 92 }, { "Bio", 91 }, { "Social", 91 } }},
+    new Student {First="Michael", Last="Fastbender", ID=122, Scores= new Dictionary<string,int> { { "Maths", 94 }, { "Science", 92 }, { "Bio", 91 }, { "Social", 91 } }}
 };
         }
     }
--- LINQ/LinqPlayground/LinqPlayground/Program.cs
+++ LINQ/LinqPlayground/LinqPlayground/Program.cs
@@ -21,31 +21,70 @@ namespace ConsoleApp1
             //                        select student.First + " " + student.Last;
 
             // Projection transformation to annonymous collection.
+
             var qualifiedStudents = from student in studentsQuery
-                                    select new { student.First, Name = student.First + " " + student.Last, Average = student.Scores.Average() };
+                                    select new { student.First, Name = student.First + " " + student.Last, Average = student.Scores.Values.Average() };
 
             var qualifiedStudent = qualifiedStudents.FirstOrDefault();
 
-            Console.Write(qualifiedStudent?.Name + " " + qualifiedStudent?.Average + "\n");
+            Console.Write($"\n\t\tTop performer is: {qualifiedStudent?.Name} with average of {qualifiedStudent?.Average}\n\n");
 
 
             //// First OR Default
             //var firstStudent = students.FirstOrDefault();
             //Console.WriteLine($"{firstStudent?.First} {firstStudent?.Last}");
 
-
+            Console.WriteLine("");
             foreach (var student in studentsQuery)
             {
-                Console.WriteLine($"{student.First} {student.Last} {student.Scores.Average()}");
+                Console.WriteLine($"{student.First} {student.Last} {student.Scores.Values.Average()}");
             }
             Console.WriteLine(studentsQuery.Count());
+
+
+
+            var studentMaxMarksInMath = from maxMathMarks in students
+                                        orderby maxMathMarks.Scores["Maths"] descending
+                                        select new { Name = maxMathMarks.First + " " + maxMathMarks.Last ,HighestMathMarks = maxMathMarks.Scores["Maths"] };
+            var mathTopperMarks = 0;
+            Console.WriteLine("\n\n\nMaths Topper:\n");
+            foreach (var maxMarks in studentMaxMarksInMath)
+            {
+                if (maxMarks.HighestMathMarks>= mathTopperMarks)
+                {
+                    mathTopperMarks = maxMarks.HighestMathMarks;
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine($"{maxMarks.Name} {mathTopperMarks}");
+            }
+            var studentMaxMarksInBio = from maxBioMarks in students
+                                       orderby maxBioMarks.Scores["Bio"] descending
+                                       select new { Name = maxBioMarks.First + " " + maxBioMarks.Last, HighestBioMarks = maxBioMarks.Scores["Bio"] };
+            var bioTopperMarks = 0;
+            Console.WriteLine("\n\n\nBio Topper:\n");
+            foreach (var maxMarks in studentMaxMarksInBio)
+            {
+                if (maxMarks.HighestBioMarks >= bioTopperMarks)
+                {
+                    bioTopperMarks = maxMarks.HighestBioMarks;
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine($"{maxMarks.Name} {bioTopperMarks}");
+            }
         }
+
 
         public static IEnumerable<Student> GetStudentsWithQuerySyntax(int rowCount, int pageIndex)
         {
             var skipRows = (pageIndex - 1) * rowCount;
             return (from student in students
-                    let average = student.Scores.Average()
+                    let average = student.Scores.Values.Average()
                     orderby average descending
                     select student).Skip(skipRows).Take(rowCount);
         }
@@ -53,9 +92,10 @@ namespace ConsoleApp1
         public static IEnumerable<Student> GetStudentsWithMethodSyntax(int rowCount, int pageIndex)
         {
             var skipRows = (pageIndex - 1) * rowCount;
-            return students.OrderBy(st => st.First).OrderBy(st=>st.Last).MySkip(skipRows).MyTake(rowCount);
+            return students.OrderBy(st => st.First).OrderBy(st => st.Last).MySkip(skipRows).MyTake(rowCount);
         }
     }
+
     static class StudentExtensions
     {
         public static IEnumerable<Student> MySkip(this IEnumerable<Student> students, int value)
